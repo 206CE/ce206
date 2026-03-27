71bdfca2a073d21fb8fb32add8c43ee06c605d9f
JohannesBotha <jacobotha206@gmail.com>
2026-03-27 15:01:16 +0200

    1

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
 export { Logo } from './1_1_Logo';
-export { Navigation } from './1_2_Navigation';
 export {DesktopMenu} from './1_2_DesktopMenu';
-export {MobileMenu} from './1_2_MobileMenu';
-export { Social } from './1_3_Social';
+export {MobileMenu} from './1_3_MobileMenu';
+export { Social } from './2_2_Social';
 export { CopyRight } from './1_4_CopyRight';
 export { Hero } from './1_5_Hero';
 export { Section } from './1_6_Section';
@@ -11,8 +10,9 @@ export { About } from './1_8_About';
 export { Contacts } from './1_9_Contacts';
 
 export { Profile } from './2_1_Profile';
-export { CtaBar } from './2_2_CtaBar';
-export { TwoColumnSplit } from './2_3_TwoColumnSplit';
+
+export { CtaBar } from './2_3_CtaBar';
+export { TwoColumnSplit } from './2_4_TwoColumnSplit';
 
 export { BackNav } from './12_BackNav';
 
